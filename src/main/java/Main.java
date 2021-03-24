7bde993dba413156fd07eb4438e383ccd743e52f
AAidoo.OptiMates <andy.aidoo@optimates.ch>
2021-03-24 17:29:25 +0100

    creating and packaging maven project works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import org.apache.maven.project.MavenProject;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
@@ -18,8 +20,8 @@ public class Main {
     private static void runProcess(String command, String fileDirectory) throws Exception{
         Process process = Runtime.getRuntime().exec(command, null, new File(fileDirectory));
 
-        printLines(command + "stdout:", process.getInputStream());
-        printLines(command + "stderr:", process.getErrorStream());
+        printLines(command + " stdout:", process.getInputStream());
+        printLines(command + " stderr:", process.getErrorStream());
         process.waitFor();
         System.out.println(command + " exitValues() " + process.exitValue());
     }
@@ -27,16 +29,25 @@ public class Main {
     public static void main(String[] args){
         try{
 
+            // path of where project folder is
             String path = "C:/Users/LenovoThinkPadT450s/OneDrive - Universität Zürich UZH/Studium/Bachelor Thesis/" +
                     "Repository mining/SourceCodeAnalyzer/sample_projects/basic_mvn_project";
 
-            String fileDirectoryCreateMavenProject = path;
-            String createMavenProject = "mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false";
 
-            String fileDirectoryPackageMavenProject = "../sample_projects/my-app";
-            String packageMavenProject = "mvn package";
 
-            runProcess(createMavenProject, fileDirectoryCreateMavenProject);
+            // create new maven project
+            String createMavenProject = "cmd.exe /c mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false";
+
+            // package new maven project
+            String fileDirectoryPackageMavenProject = path + "/my-app";
+            String packageMavenProject = "cmd.exe /c mvn clean package";
+
+            // execute command to create new maven project
+            runProcess(createMavenProject, path);
+
+            // execute command to package maven project
+            runProcess(packageMavenProject, fileDirectoryPackageMavenProject);
+
 
         } catch(Exception e) {
             e.printStackTrace();
